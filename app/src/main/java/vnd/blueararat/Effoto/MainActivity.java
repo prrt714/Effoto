33e7df29fbf89a8a4c454463fbe172dd392d9126
prrt714 <prrt714@gmail.com>
2018-10-28 16:31:09 +0300

    [x] larger icon

--- app/src/main/java/vnd/blueararat/Effoto/MainActivity.java
+++ app/src/main/java/vnd/blueararat/Effoto/MainActivity.java
@@ -36,7 +36,6 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ShareCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
